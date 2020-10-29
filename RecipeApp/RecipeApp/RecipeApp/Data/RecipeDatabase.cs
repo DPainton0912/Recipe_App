ba02c16687d7b538e69c0e34120d2d3ab24a12ac
DPainton0912 <dgpainton@gmail.com>
2020-10-30 12:27:45 +1300

    Added model and database.

---
+++ RecipeApp/RecipeApp/RecipeApp/Data/RecipeDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using RecipeApp.Models;
+
+namespace RecipeApp.Data
+{
+    class RecipeDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public RecipeDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Recipe>().Wait();
+        }
+
+        public Task<List<Recipe>> GetRecipeAsync()
+        {
+            return _database.Table<Recipe>().ToListAsync();
+        }
+
+        public Task<Recipe> GetRecipeAsync(int id)
+        {
+            return _database.Table<Recipe>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveRecipeAsync(Recipe recipe)
+        {
+            if (recipe.ID != 0)
+            {
+                return _database.UpdateAsync(recipe);
+            }
+            else
+            {
+                return _database.InsertAsync(recipe);
+            }
+        }
+
+        public Task<int> DeleteRecipeAsync(Recipe recipe)
+        {
+            return _database.DeleteAsync(recipe);
+        }
+    }
+}
---
+++ RecipeApp/RecipeApp/RecipeApp/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace RecipeApp.Models
+{
+    class Recipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Created { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+    }
+}
